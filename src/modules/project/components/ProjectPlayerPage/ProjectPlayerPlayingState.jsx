3c583b02000a8ec245f1639c36a0612db9ab6b58
magicpalmtree <magicpalmtree.space@gmail.com>
2022-12-08 09:29:04 -0500

    text commit2

--- src/modules/project/components/ProjectPlayerPage/ProjectPlayerPlayingState.jsx
+++ src/modules/project/components/ProjectPlayerPage/ProjectPlayerPlayingState.jsx
@@ -4,7 +4,7 @@ import { animationState, preAnimationState, transition } from "../../../../compo
 import Button from "../../../../components/Buttons/Button";
 import { BooleanInput, Option } from "../../../../components/PropertyEditor";
 // import ColorPicker from "../../../../components/ColorPicker";
-import DropImageZone from "../../../media/components/DropImageZone";
+// import DropImageZone from "../../../media/components/DropImageZone";
 
 const ProjectPlayerPlayingState = ({project, update, updateSkin , updateEditingStatus}) => {
 
@@ -42,7 +42,7 @@ const ProjectPlayerPlayingState = ({project, update, updateSkin , updateEditingS
               stackOrder={2}
               onChange={updateSkin('background', {controls: true})}
             /> */}
-            <div className="form-control">
+            {/* <div className="form-control">
               <label>Branding Image</label>
               <DropImageZone
                 onSuccess={({src}) => updateProject('branding_image_src', src)}
@@ -50,7 +50,7 @@ const ProjectPlayerPlayingState = ({project, update, updateSkin , updateEditingS
                 directory="brandingImages"
               />
               {!!project.branding_image_src && <Button onClick={() => update('branding_image_src', null)} style={{marginTop: '10px'}}>Clear Image</Button>}
-            </div>
+            </div> */}
         </div>
       </motion.div>
     </AnimatePresence>
